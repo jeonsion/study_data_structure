666f9589c8b22bb8428f9e55118aa1eb83df0534
Sion Jeon <a24349663@gmail.com>
2022-05-30 00:05:24 +0900

    0530

--- 2021test01.c
+++ 2021test01.c
@@ -10,22 +10,30 @@ typedef struct node{
 
 
 int main()  {
-    Node*head = NULL;   //첫번째 노드를 가리키는 head저장
+    Node* p = NULL;   //마지막 노드를 가리키는 head저장
 
-    Node *n1 = (Node*)malloc(sizeof(Node));
-    n1->a = "I"
+    Node * n1 = (Node *)malloc(sizeof(Node));
+    n1->a = 'I';
     n1->llink = NULL;
-    n1 ->rlink = NULL;
+    n1->rlink = NULL;
+    p = n1;
 
-    Node *n2 = (Node*)malloc(sizeof(Node));
-    n2->a = "o"
-    n2->llink = NULL;
-    n2 ->rlink = NULL;
+    Node * n2 = (Node *)malloc(sizeof(Node));
+    n2->a = 'o';
+    n2->rlink = p->rlink;
+    p->rlink = n2;
+    p->rlink->llink = p;
+    p=n2;
 
+    Node * n3 = (Node *)malloc(sizeof(Node));
+    n3->a = 'T';
+    n3->rlink = p->rlink;
+    p->rlink = n3;
+    p->rlink->llink = p;
+    p=n3;
 
-
-    Node *n3 = (Node*)malloc(sizeof(Node));
-    n3->a = "T"
-    n3->llink = NULL;
-    n3 ->rlink = NULL;
+    while(p!=NULL)  {
+        printf("%c", p->a);
+        p=p->llink;
+    }
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,6 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-    int col;
-} element;
+typedef struct node{
+    char a;
+    struct node *llink;
+    struct node *rlink;
+}Node;
 
-typedef struct {
-    element data[MAX_STACK_SIZE];
+void print_reverse(p) {
+    while(p!=NULL)  {
+        printf("%c", p->data);
+        p=p->llink;
+    }
+}
+
+int main()  {
+    Node* p = NULL;   //마지막 노드를 가리키는 head저장
+
+    Node * new_node = (Node *)malloc(sizeof(Node));
+    new_node->a = 'I';
+    new_node->llink = NULL;
+    new_node->rlink = NULL;
+    p = new_node;
+
+    Node * new_node = (Node *)malloc(sizeof(Node));
+    new_node->a = 'o';
+    new_node->rlink = p->rlink;
+    p->rlink = new_node;
+    p->rlink->llink = p;
+    p=new_node;
+
+    Node * new_node = (Node *)malloc(sizeof(Node));
+    new_node->a = 'T';
+    new_node->rlink = p->rlink;
+    p->rlink = new_node;
+    p->rlink->llink = p;
+    p=new_node;
+
+    print_reverse();
+}
